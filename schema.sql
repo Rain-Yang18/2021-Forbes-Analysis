5498102077961b4cfa3f74894d912bc12f8a87cf
RAIN YANG <RainYang@192-168-1-5.tpgi.com.au>
2021-06-03 22:34:46 +1000

    Create Draft branch to store ETL process

---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- Drop tables if exists
+DROP TABLE IF EXISTS forbes_billionaires;
+
+-- Create One table
+CREATE TABLE forbes_billionaires (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	networth INTEGER,
+	country TEXT,
+	source TEXT,
+	rank INTEGER,
+	age INTEGER,
+	citizenship TEXT,
+	status TEXT,
+	children INTEGER,
+	self_made TEXT,
+	degree TEXT,
+	university TEXT,
+	longitude TEXT,
+	latitude TEXT
+);
+
+SELECT * FROM forbes_billionaires;
