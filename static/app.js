a1dfeaec407675e254d3032851ea70c434384e98
RAIN YANG <RainYang@192-168-1-5.tpgi.com.au>
2021-06-07 20:08:25 +1000

    Update

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 // Import our CSV data with d3's .csv import method.
-d3.json("/test").then(function(data) {
+d3.json("/forbes").then(function(data) {
   // Visualize the data
   console.log(data);
   // visualize(data);
