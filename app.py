cdb594241a52dbe760539bc9179eb20778c6d5e8
mhaszek <32060715+mhaszek@users.noreply.github.com>
2021-06-08 19:06:27 +1000

    test

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from config import (username, password)
 
 # from flask import Flask, jsonify
 
